fd2d00f025f6a213a938c3f7005eb5a4166a7615
Daniela Rodriguez <daniela.rodriguez@banistmo.com>
2021-04-05 12:21:57 -0500

    Manejo de iconos propios y de terceros

--- my_flutter_app_2/lib/page/home_page.dart
+++ my_flutter_app_2/lib/page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../widgets/circle_container.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -10,7 +11,15 @@ class HomePage extends StatelessWidget {
         title: Text("App cool"),
       ),
       body: Center(
-        child: CircleContainer(child: Text("Holi"), width: 100, height: 100),
+        child: CircleContainer(
+            child: SvgPicture.asset(
+              "assets/icons/menu.svg",
+              width: 30,
+              height: 30,
+              color: Colors.blue,
+            ),
+            width: 100,
+            height: 100),
       ),
     );
   }
